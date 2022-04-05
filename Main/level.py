a71bff7d3a6b532d687961346bceb4cd0a7fbaf6
Kuntz <kuntz@Kuntz.VivoBook>
2022-04-05 14:16:20 +0200

    made player, trying to resolve level not seing player.py file

--- Main/level.py
+++ Main/level.py
@@ -1,4 +1,7 @@
 import pygame
+from settings import *
+from tile import Tile
+
 
 class Level:
     def __init__(self):
@@ -9,6 +12,17 @@ class Level:
         # Sprite group setup
         self.visible_sprites = pygame.sprite.Group()
         self.obstacles_sprites = pygame.sprite.Group()
+
+        # Sprite Setup
+        self.create_map()
+
+    def create_map(self):
+        for row_index,row in enumerate(WORLD_MAP):
+            for col_index, col in enumerate(row):
+                x = col_index * TILESIZE
+                y = row_index * TILESIZE
+                if col == 'x':
+                    Tile((x,y),[self.visible_sprites])
 
     def run(self):
         # Update and draw the game
--- Main/tile.py
+++ Main/tile.py
@@ -4,5 +4,5 @@ from settings import *
 class Tile(pygame.sprite.Sprite):
     def __int__(self, pos, groups):
         super().__init__(groups)
-        self.image = pygame.image.load('rock.png location').convert_alpha()
+        self.image = pygame.image.load('../Graphics/FX/Particle/Rock.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
